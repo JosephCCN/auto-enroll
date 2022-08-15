6291436779c3fd6df7f4ec7320d9764c602776e2
Mariouz <joseph02252003@gmail.com>
2022-08-15 18:01:53 +0800

    able to login cusis

--- auto.js
+++ auto.js
@@ -1,14 +1,35 @@
 const puppeteer = require('puppeteer');
+require('dotenv').config();
 
 const main = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:5000');
+    await page.goto('https://cusis.cuhk.edu.hk/psp/CSPRD/?cmd=login&languageCd=ENG&');
     //await page.screenshot({path: 'button.png'});
-    const t = Date.parse('14 Aug 2022 12:51:00 GMT') - Date.now();
+    /*const t = Date.parse('15 Aug 2022 09:27:00 GMT') - Date.now();
     console.log(t);
-    await page.waitForTimeout(t);
-    page.click('[id="button"]'); 
+    await page.waitForTimeout(t);*/
+    await page.type('#userNameInput', process.env.USRNAME);
+    await page.type('#passwordInput', process.env.PW);
+
+    //await page.keyboard.press('Enter'); 
+
+
+    await page.evaluate(() => {
+        console.log('doing sth');
+        Array.from(document.querySelectorAll('span')).filter(li => {
+            console.log(li.innerText);
+            return li.innerText == 'Sign in'
+        }).forEach(ele => {
+            if (ele) ele.click();
+        });
+    });
+
+
+    await page.waitForTimeout(2000);
+    
+    await page.screenshot({path: "cusis.png"});
+
 
     await browser.close();
 };

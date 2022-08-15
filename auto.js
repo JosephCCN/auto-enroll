3f9c285c2fe48948540ff23c7abdd5af01f0ddd1
Mariouz <joseph02252003@gmail.com>
2022-08-15 18:38:25 +0800

    workable, but need test

--- auto.js
+++ auto.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const main = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://cusis.cuhk.edu.hk/psp/CSPRD/?cmd=login&languageCd=ENG&');
+    await page.goto('https://cusis.cuhk.edu.hk/psc/CSPRD_2/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_SHOP_CART_FL.GBL?Page=SSR_SHOP_CART_FL');
     //await page.screenshot({path: 'button.png'});
     /*const t = Date.parse('15 Aug 2022 09:27:00 GMT') - Date.now();
     console.log(t);
@@ -12,24 +12,28 @@ const main = async () => {
     await page.type('#userNameInput', process.env.USRNAME);
     await page.type('#passwordInput', process.env.PW);
 
-    //await page.keyboard.press('Enter'); 
+    await page.keyboard.press('Enter'); 
+    //login to cusis alr
+    
+    await page.waitForTimeout(3000);
 
+    await page.click('[id="DERIVED_REGFRM1_SSR_SELECT$0"]');
+    await page.click('[id="DERIVED_REGFRM1_SSR_SELECT$2"]');
+    await page.click('[id="DERIVED_REGFRM1_SSR_SELECT$4"]');
 
     await page.evaluate(() => {
-        console.log('doing sth');
-        Array.from(document.querySelectorAll('span')).filter(li => {
-            console.log(li.innerText);
-            return li.innerText == 'Sign in'
+        Array.from(document.querySelectorAll('a')).filter(li => {
+            return li.innerText == 'Validate'
         }).forEach(ele => {
-            if (ele) ele.click();
-        });
+            if(ele) ele.click();
+        })
     });
 
+    await page.waitForTimeout(3000);
 
-    await page.waitForTimeout(2000);
-    
-    await page.screenshot({path: "cusis.png"});
+    await page.setViewport({width: 900, height: 1000});
 
+    await page.screenshot({path: 'cusis.png'});
 
     await browser.close();
 };
